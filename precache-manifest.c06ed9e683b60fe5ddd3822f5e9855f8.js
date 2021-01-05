5d287257fa8d15d74b1f3ff51b0d7bff0c2d6af2
Giorgi <khukhunaishvili@gmail.com>
2021-01-05 22:41:32 +0400

    deploy

---
+++ precache-manifest.c06ed9e683b60fe5ddd3822f5e9855f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d799744a12e94a728a8",
+    "url": "/githubers/css/app.0dc52feb.css"
+  },
+  {
+    "revision": "782c99732169c6a47521f3685c09b169",
+    "url": "/githubers/index.html"
+  },
+  {
+    "revision": "1d799744a12e94a728a8",
+    "url": "/githubers/js/app.d49e19ab.js"
+  },
+  {
+    "revision": "ac1c88b1de6f4af82793",
+    "url": "/githubers/js/chunk-vendors.fa193a8a.js"
+  },
+  {
+    "revision": "8b170db84cc995b2e000b8fa7eb24292",
+    "url": "/githubers/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/githubers/robots.txt"
+  }
+]);
