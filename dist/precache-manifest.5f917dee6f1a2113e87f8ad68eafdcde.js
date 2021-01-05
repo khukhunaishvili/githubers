87e9cb47436033980e536bf281ff1b4407de5ff5
Giorgi <khukhunaishvili@gmail.com>
2021-01-05 21:03:06 +0400

    Initial dist subtree commit

---
+++ dist/precache-manifest.5f917dee6f1a2113e87f8ad68eafdcde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aae9157bf043f1e4b4e",
+    "url": "/githubers/css/app.0dc52feb.css"
+  },
+  {
+    "revision": "27803a8d9507a1b4947f90e6ffc92601",
+    "url": "/githubers/index.html"
+  },
+  {
+    "revision": "2aae9157bf043f1e4b4e",
+    "url": "/githubers/js/app.0b77f89d.js"
+  },
+  {
+    "revision": "ac1c88b1de6f4af82793",
+    "url": "/githubers/js/chunk-vendors.fa193a8a.js"
+  },
+  {
+    "revision": "8b170db84cc995b2e000b8fa7eb24292",
+    "url": "/githubers/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/githubers/robots.txt"
+  }
+]);
